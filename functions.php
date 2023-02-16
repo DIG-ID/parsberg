1964365f92455a345b66cc65668fb73bead6729a
Daniel-Digid <daniel@dig.id>
2023-02-16 19:10:06 +0000

    dev

--- functions.php
+++ functions.php
@@ -276,7 +276,7 @@ function parsberg_theme_enqueue_styles() {
 
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/dist/js/app.js', array( 'jquery' ), $theme_version, false );
-	if ( is_page_template( 'page-templates/page-contact.php' ) ) :
+	if ( is_page_template( 'page-templates/page-contact.php' ) || is_admin() ) :
 		wp_enqueue_script( 'google-map-settings', get_stylesheet_directory_uri() . '/assets/js/google-maps.js', array( 'jquery' ), $theme_version, true );
 		wp_enqueue_script( 'google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCB2RShyxiN7xPsQy1QI_SbqXXjW5p08S0&callback=initMap', array(), $theme_version, true );
 	endif;
@@ -287,6 +287,14 @@ function parsberg_theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'parsberg_theme_enqueue_styles' );
+
+//Google Map Init
+function my_acf_init() {
+	if ( is_admin() ) :
+    	acf_update_setting('google_api_key', 'AIzaSyCB2RShyxiN7xPsQy1QI_SbqXXjW5p08S0');
+	endif;
+}
+add_action('acf/init', 'my_acf_init');
 
 /**
  * Wrap the post thumbnail image in a figure element only in the blog posts and project posts.
