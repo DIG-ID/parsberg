e5468012fe66f9bdc38466153a85e6b0acc12aa3
Daniel-Digid <daniel@dig.id>
2023-02-17 14:43:02 +0000

    dev

--- template-parts/pages/terms-conditions/content.php
+++ template-parts/pages/terms-conditions/content.php
@@ -1,4 +1,4 @@
-<section class="terms__content-section pt-20 pb-40">
+<section class="terms__content-section pb-40">
     <div class="container px-8 lg:px-0 grid grid-cols-1 lg:grid-cols-3 lg:mx-auto">
         <div class="col-span-2">
         <?php the_content(); ?>
