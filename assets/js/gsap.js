f8cf39094e0a39ba1410df84aecad23aa07c053e
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-02-23 10:32:28 +0000

    up

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -13,14 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
       herotl.to(".section-hero-mask", {duration: 2, delay: 1, clipPath: 'inset(0 0 100% 0)'});
       herotl.from(".section-hero-scroll", {duration: 2, y: 50, opacity: 0}, "<");
       herotl.from(".section-hero-title", {duration: 2, y: 50, opacity: 0}, "<0.5");
-      herotl.from(".button, .site-logo", {duration: 1.5, y: 50, opacity: 0, stagger: 0.15}, "<0.5");
+      herotl.from(".button, #site-logo-wrapper", {duration: 1.5, y: 50, opacity: 0, stagger: 0.15}, "<0.5");
 
       // Location Section
-      let locationtl = gsap.timeline({ defaults: { duration: 0.6 } });
-      
-      
+      let locationtl = gsap.timeline({ defaults: { duration: 1 } });
+
       locationtl.from('.section-location svg', { y: '-50px', opacity: 0 });
-      locationtl.from('.section-location span', { duration: 2, opacity: 0 }, '<');
+      locationtl.from('.section-location span', { opacity: 0 }, '<');
       locationtl.from('.section-location p', { y: '-50px', opacity: 0 });
 
       let locationSt = ScrollTrigger.create({
@@ -39,13 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let togglerTl = gsap.timeline();
     togglerTl.to('.main-header', {duration: 0.5, backgroundColor: 'var(--main-green)', minHeight: '100vh'});
-    togglerTl.to('.navigation-mobile', {duration: .5, y: '0%', opacity: 1}, '<0.5');
-    togglerTl.from('.nav-link', {duration: .6, x: '-50px', opacity: 0, stagger: 0.15 }, '<0.25');
+    togglerTl.to('.navigation-mobile', {duration: 0.5, y: '0%', opacity: 1}, '<0.25');
+    togglerTl.from('.navigation-mobile .nav-link', {duration: 0.6, x: '-50px', opacity: 0, stagger: 0.15 }, '<0.25');
     togglerTl.paused(true);
 
     toggleBtn.addEventListener( 'click', (e) => {
       //toggleBtn.classList.toggle('main-header--open');
       document.body.classList.toggle('menu-open');
+      
       if (togglerTl.paused()) {
         togglerTl.play();
       } else if (togglerTl.reversed()) {
