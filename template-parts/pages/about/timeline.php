a3ce87299ae48896b9a87f8f43d03400cd60dcc6
Daniel-Digid <daniel@dig.id>
2023-05-10 13:02:53 +0100

    dev

--- template-parts/pages/about/timeline.php
+++ template-parts/pages/about/timeline.php
@@ -7,7 +7,7 @@
         <div class="timeline-description__col invisible relative col-span-1 lg:col-span-5 xl:col-start-2 xl:col-span-3 lg:pl-7 lg:pr-20">
             <p class="mb-8 leading-8"><?php the_field('timeline_section_description'); ?></p>
         </div>
-        <div class="col-span-1 lg:col-span-7 xl:col-span-8 lg:border-l lg:border-l-[#707070]">
+        <div class="timeline-description__col-content relative col-span-1 lg:col-span-7 xl:col-span-8 lg:border-l lg:border-l-[#707070]">
         <?php
         if( have_rows('timeline_section_timeline_list') ):
         while( have_rows('timeline_section_timeline_list') ) : the_row(); ?>
