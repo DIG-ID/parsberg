c4da504e57fbfaf4cdc4f865045d906b8548c4ab
Daniel-Digid <daniel@dig.id>
2023-05-11 17:22:33 +0100

    dev

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+  //Remove scroll from body when menu is open
+
+  $('.menu-button').click(function() {
+    if ($('body').hasClass('menu-open')) {
+      $('body').removeClass("fixed");
+    } else {
+      $('body').addClass("fixed");
+    }
+  });
   //Sticky navigation
   const header = document.querySelector('.main-header');
   let lastScroll = 0;
