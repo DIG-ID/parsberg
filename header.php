bd50037095a5e3be238529d459db005f0ff2e649
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-05-24 16:33:03 +0100

    up

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <html <?php language_attributes(); ?>>
 
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<script>
 			window.dataLayer = window.dataLayer || [];
 			function gtag() {
@@ -21,6 +19,8 @@
 			gtag("set", "ads_data_redaction", true);
 			gtag("set", "url_passthrough", true);
 		</script>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<?php wp_head(); ?>
 	</head>
 
